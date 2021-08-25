3a09772d0be001354d684250abebf06cffffe970
son.giang <22.sonstarx.22@gmail.com>
2021-08-25 22:05:47 +0700

    ontap

---
+++ tuan3/th-upload file/src/main/java/model/Product.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Product {
+    private String name;
+    private boolean price;
+    private String img;
+
+    public Product(String name, boolean price, String img) {
+        this.name = name;
+        this.price = price;
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPrice() {
+        return price;
+    }
+
+    public void setPrice(boolean price) {
+        this.price = price;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
